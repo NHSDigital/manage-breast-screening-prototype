e881b8918c9208613aad1e4b007f2d0261ef5a79
Ed Horsford <mail@edwardhorsford.com>
2025-10-24 17:02:34 +0100

    Get editing and deleting previous mammograms working

--- app/data/users.js
+++ app/data/users.js
@@ -24,5 +24,45 @@ module.exports = [
     email: 'c.lowen45@nhs.net',
     id: 'e1945412-aad7-46a2-a984-d4f6f654c229',
     breastScreeningUnit: 'm5ekcxvu'
+  },
+  {
+    firstName: 'Claudia',
+    lastName: 'Patterson',
+    role: ['clinician'],
+    email: 'm.patterson12@nhs.net',
+    id: '7b2d8f4e-3c1a-4e9b-8f6d-2a5c9e1b4d3f',
+    breastScreeningUnit: 'm5ekcxvu'
+  },
+  {
+    firstName: 'Priya',
+    lastName: 'Sharma',
+    role: ['clinician'],
+    email: 'p.sharma8@nhs.net',
+    id: 'c4e6f2a8-9d5b-4c3e-a1f7-8b2d4e6c9a1b',
+    breastScreeningUnit: 'm5ekcxvu'
+  },
+  {
+    firstName: 'Agatha',
+    lastName: 'Thompson',
+    role: ['clinician'],
+    email: 'd.thompson23@nhs.net',
+    id: 'f3a1c5e7-2b4d-4a6e-9c8f-1d3b5e7a9c2d',
+    breastScreeningUnit: 'm5ekcxvu'
+  },
+  {
+    firstName: 'Rachel',
+    lastName: 'Bennett',
+    role: ['administrative'],
+    email: 'r.bennett17@nhs.net',
+    id: 'a9c2e4f6-1d3b-4e5c-8a7f-2c4d6e8b1a3c',
+    breastScreeningUnit: 'm5ekcxvu'
+  },
+  {
+    firstName: 'Mohammed',
+    lastName: 'Ali',
+    role: ['administrative'],
+    email: 'm.ali34@nhs.net',
+    id: 'e2d4f6a8-3c5b-4e7d-9a1c-6b8e2d4f6a9c',
+    breastScreeningUnit: 'm5ekcxvu'
   }
 ]
