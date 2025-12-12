b67df662f0898bb5d6317ab408bbfb0b58ab8598
Ed Horsford <mail@edwardhorsford.com>
2025-12-12 10:11:58 +0000

    Use BSU name in places

--- app/locals.js
+++ app/locals.js
@@ -1,13 +1,19 @@
 // app/locals.js
 
 module.exports = (config) => (req, res, next) => {
+  const currentUser = req.session.data.currentUser
+  const currentBSU = currentUser
+    ? req.session.data.breastScreeningUnits?.find(unit => unit.id === currentUser.breastScreeningUnit)
+    : null
+
   const locals = {
     serviceName: config.serviceName,
     currentUrl: req.path,
     flash: req.flash(),
     query: req.query,
     referrerChain: req.query.referrerChain,
-    currentUser: req.session.data.currentUser,
+    currentUser: currentUser,
+    currentBSU: currentBSU,
     isAppointmentWorkflow: false
   }
 
