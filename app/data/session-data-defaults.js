b1ed1c2876e58e6927f12d6d2e4d281d502b32ca
Ed Horsford <mail@edwardhorsford.com>
2026-05-26 16:53:42 +0100

    Clean up code

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -114,7 +114,7 @@ const defaultSettings = {
     showRemaining: 'true',
     autoOpenPacsViewer: 'false',
     enableOpinionDelay: 'true',
-    annotationsMode: 'with-images', // 'without-images' | 'with-images-simple' | 'with-images' | 'with-images-progressive' | 'opinions-in-tabs'
+    annotationsMode: 'with-images', // 'without-images' | 'with-images-simple' | 'with-images' | 'with-images-progressive'
     secondReaderComparison: 'late', // 'early' | 'late' | 'off'
     compareWhen: 'non_normal', // 'non_normal' | 'discordant_only'
     arbitrationPolicy: 'discordant_only', // 'discordant_only' | 'all_non_normal'
