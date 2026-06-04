69db1edff7aabe1157cb9a05d902fce00682b93e
Ed Horsford <mail@edwardhorsford.com>
2026-06-04 10:14:23 +0100

    Add explanatory comments

--- app/data/abnormality-types.js
+++ app/data/abnormality-types.js
@@ -1,14 +1,15 @@
 // app/data/abnormality-types.js
 
 module.exports = [
-  // Order chosen specifically
+  // Features seen in breast tissue, types ordered specifically
   'Ill-defined mass',
   'Well-defined mass',
   'Microcalcification',
   'Asymmetric density',
   'Architectural distortion',
 
-  // 'Clinical abnormality',
+  // General features / other
+  // 'Clinical abnormality', // clinical likely not needed as we have a separate option for this
   'Lymph node',
   'Other'
 ]
