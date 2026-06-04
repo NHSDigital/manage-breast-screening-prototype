59fbea2415717e0a68bfd135cb73fc9f1f83a129
Ed Horsford <mail@edwardhorsford.com>
2026-06-04 12:46:11 +0100

    Update annotation form following SME call (#323)

--- app/data/abnormality-types.js
+++ app/data/abnormality-types.js
@@ -1,12 +1,15 @@
 // app/data/abnormality-types.js
 
 module.exports = [
-  'Architectural distortion',
-  'Asymmetric density',
-  'Clinical abnormality',
-  'Lymph node abnormality',
-  'Mass ill-defined',
-  'Mass well-defined',
+  // Features seen in breast tissue, types ordered specifically
+  'Ill-defined mass',
+  'Well-defined mass',
   'Microcalcification',
+  'Asymmetric density',
+  'Architectural distortion',
+
+  // General features / other
+  // 'Clinical abnormality', // clinical likely not needed as we have a separate option for this
+  'Lymph node',
   'Other'
 ]
