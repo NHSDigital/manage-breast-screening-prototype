02aff6c726bfc30103ede442880f08a891adddcf
rivalee <r.e.cottrell@gmail.com>
2026-08-06 11:58:13 +0100

    Update check info page

--- app/lib/utils/medical-information.js
+++ app/lib/utils/medical-information.js
@@ -476,9 +476,9 @@ const summariseBreastDensityFactors = (medicalInformation) => {
   const summaries = []
 
   if (hrt === 'yes') {
-    summaries.push('Taking HRT')
+    summaries.push('Begun a course of HRT since last screening appointment')
   } else if (hrt === 'no') {
-    summaries.push('Not taking HRT')
+    summaries.push('Not begun a course of HRT since last screening appointment')
   }
 
   if (factors.includes('pregnant')) {
